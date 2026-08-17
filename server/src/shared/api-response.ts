6d4485b79ffe4b499b77adda3d19c26691f3d756
Jean Carlos <jeanccdev@gmail.com>
2026-08-17 04:50:21 -0300

    feat: mais métodos de api response

--- server/src/shared/api-response.ts
+++ server/src/shared/api-response.ts
@@ -48,7 +48,80 @@ export class ApiResponse {
     };
   }
 
-  static error <T = null>(
+  static noContent<T = null>(message: string, data: T): SimpleApiResponse<T> {
+    return {
+      status: 204,
+      message,
+      data,
+    };
+  }
+
+  static badRequest<T = null>(message: string, data: T): SimpleApiResponse<T> {
+    return {
+      status: 400,
+      message,
+      data,
+    };
+  }
+
+  static unauthorized<T = null>(
+    message: string,
+    data: T,
+  ): SimpleApiResponse<T> {
+    return {
+      status: 401,
+      message,
+      data,
+    };
+  }
+
+  static forbidden<T = null>(message: string, data: T): SimpleApiResponse<T> {
+    return {
+      status: 403,
+      message,
+      data,
+    };
+  }
+
+  static notFound<T = null>(message: string, data: T): SimpleApiResponse<T> {
+    return {
+      status: 404,
+      message,
+      data,
+    };
+  }
+
+  static conflict<T = null>(message: string, data: T): SimpleApiResponse<T> {
+    return {
+      status: 409,
+      message,
+      data,
+    };
+  }
+
+  static unprocessableEntity<T = null>(
+    message: string,
+    data: T,
+  ): SimpleApiResponse<T> {
+    return {
+      status: 422,
+      message,
+      data,
+    };
+  }
+
+  static internalServerError<T = null>(
+    message: string,
+    data: T,
+  ): SimpleApiResponse<T> {
+    return {
+      status: 500,
+      message,
+      data,
+    };
+  }
+
+  static error<T = null>(
     status: number,
     message: string,
     data: T,
@@ -56,7 +129,7 @@ export class ApiResponse {
     return {
       status,
       message,
-      data, 
+      data,
     };
   }
 }
